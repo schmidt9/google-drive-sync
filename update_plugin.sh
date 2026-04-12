a28670692987bb725bf21a90666f3f1f7bb5ce97
Alexander Kormanovsky <kormanowskij@yandex.ru>
2026-04-12 22:23:21 +0300

    add script

---
+++ update_plugin.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# script for iOS iSH app
+
+# select vault with plugin here
+mount -t ios . /mnt
+
+cd mnt
+
+# cd into mounted vault found by .obsidian directory inside it
+cd "$(find . -type d -name ".obsidian" -print -quit)"
+
+# create if not exists
+mkdir -p "plugins"
+
+cd "plugins"
+
+# (re)download repo 
+rm -rf google-drive-sync
+
+git clone https://github.com/schmidt9/google-drive-sync.git
+
+echo "repo cloned"
