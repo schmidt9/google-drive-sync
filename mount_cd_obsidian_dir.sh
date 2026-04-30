10f451dedb2ff5d4790f209212fa921cc4b4e359
Alexander Kormanovsky <kormanowskij@yandex.ru>
2026-04-30 21:39:47 +0300

    split script;

---
+++ mount_cd_obsidian_dir.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# script for iOS iSH app
+
+# select vault with plugin here
+mount_dir="/mnt"
+
+mount -t ios . "$mount_dir"
+
+cd "$mount_dir"
+
+# cd into mounted vault found by .obsidian directory inside it
+obsidian_path="$(find . -type d -name ".obsidian" -print -quit)"
+
+if [ ! -d "$obsidian_path" ]; then
+  echo "Directory '.obsidian' not found"
+  exit 1
+fi
+
+cd "$obsidian_path"
--- update_plugin.sh
+++ update_plugin.sh
@@ -2,22 +2,7 @@
 
 # script for iOS iSH app
 
-# select vault with plugin here
-mount_dir="/mnt"
-
-mount -t ios . "$mount_dir"
-
-cd "$mount_dir"
-
-# cd into mounted vault found by .obsidian directory inside it
-obsidian_path="$(find . -type d -name ".obsidian" -print -quit)"
-
-if [ ! -d "$obsidian_path" ]; then
-  echo "Directory '.obsidian' not found"
-  exit 1
-fi
-
-cd "$obsidian_path"
+source ./mount_cd_obsidian_dir.sh
 
 plugin_path="plugins/google-drive-sync"
 
